aff5d9d20488598cd577c2893e41eba1ea7b26a9
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-24 16:32:02 -0600

    Add chef-plugin fixes

--- custom/plugins/chef/chef.plugin.zsh
+++ custom/plugins/chef/chef.plugin.zsh
@@ -1,13 +1,13 @@
 function _chef_command() {
   if [[ $1 == 'generate' ]]; then
     shift 1
-    nocorrect command chef generate $@ -C 'J. Morgan Lieberthal' -m 'j.morgan.lieberthal@gmail.com' -I 'mit'
+    command chef generate $@ -C 'J. Morgan Lieberthal' -m 'j.morgan.lieberthal@gmail.com' -I 'mit'
   else
-    nocorrect command chef $@
+    command chef $@
   fi
 }
 
-alias chef='_chef_command'
+alias chef='nocorrect _chef_command'
 compdef _chef_command=chef
 
 alias cg="chef generate"
