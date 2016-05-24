4428df0d5a7bc968e5dff4ffaa5565f32c70ecde
Morgan Lieberthal <morgan@jmorgan.org>
2016-05-24 10:04:02 -0600

    fix up gulp plugin

--- custom/plugins/gulp/gulp.plugin.zsh
+++ custom/plugins/gulp/gulp.plugin.zsh
@@ -10,12 +10,13 @@ _gulp_refresh () {
 _gulp_task_list_needs_refresh () {
   if [ ! -f .gulp_tasks ]; then return 0;
   else
+    local gulpfile=$(_gulpfile)
     if [[ "$OSTYPE" == darwin* ]]; then
       local accurate=$(stat -f%m .gulp_tasks)
-      local changed=$(stat -f%m gulp/tasks/**/* Gulpfile.js | sort -nr | head -n1)
+      local changed=$(stat -f%m tools/tasks/**/* "${gulpfile}" | sort -nr | head -n1)
     else
       local accurate=$(stat -c%Y .gulp_tasks)
-      local changed=$(stat -c%Y gulp/tasks/**/* Gulpfile.js | sort -nr | head -n1)
+      local changed=$(stat -c%Y gulp/tasks/**/* "${gulpfile}" | sort -nr | head -n1)
     fi
     return $(expr $accurate '>=' $changed)
   fi
@@ -25,8 +26,19 @@ _gulp_generate () {
   gulp --silent --tasks-simple | cut -d " " -f 2 > .gulp_tasks
 }
 
+_gulpfile() {
+  local possible=$(ls Gulpfile.* 2>/dev/null)
+
+  if [[ -f "${possible}" ]]; then
+    echo "${possible}" && return 0
+  else
+    return 1
+  fi
+}
+
 _gulp () {
-  if [ -f Gulpfile.js ]; then
+  local gulpfile=$(_gulpfile)
+  if [[ -f "${gulpfile}" ]]; then
     if _gulp_task_list_needs_refresh; then
       echo "\nGenerating .gulp_tasks..." > /dev/stderr
       _gulp_generate
